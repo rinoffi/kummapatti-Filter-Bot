86afa32afe7df169900cbbdaf07aebb9844da68d
rinoffi <sivaneshoffi@gmail.com>
2025-08-16 07:32:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ SEND_ALL_LIMITE = int(environ.get('SEND_ALL_LIMITE', 5))
 LIMIT_MODE = is_enabled((environ.get('LIMIT_MODE', 'True')), False)
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7189632039').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6248675084').split()]
 OWNER_USER_NAME = environ.get("OWNER_USER_NAME", "Thizz_zoro") # widout 👉 @
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002583830624').split()]
 # post channel auto post new movie
